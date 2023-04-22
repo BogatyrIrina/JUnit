a261d49d965e478e69097c74e5e67177e47e445d
admin <kochereva_irina@mail.ru>
2023-04-22 22:44:27 +0300

    начальный

--- src/main/java/com/example/junit/User.java
+++ src/main/java/com/example/junit/User.java
@@ -1,2 +1,25 @@
-package com.example.junit;public class User {
+package com.example.junit;
+
+public class User {
+    private final String login;
+    private final String email;
+
+    public User(String login, String email) throws IllegalArgumentException {
+        if (login.equals(email)) {
+            throw new IllegalArgumentException();
+        }
+        this.login = login;
+        if (!email.contains("@") || !email.contains(".")) {
+            throw new IllegalArgumentException();
+        }
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- src/test/java/com/example/junit/UserTest.java
+++ src/test/java/com/example/junit/UserTest.java
@@ -1,2 +1,29 @@
-package com.example.junit;public class UserTest {
+package com.example.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserTest {
+    @BeforeAll
+    private static void initParams(){
+        System.out.println("Start testing");
+    }
+    @AfterAll
+    private static void shouldDoAfterAll(){
+        System.out.println("Finish testing");
+    }
+    @Test
+    void shouldGetLogin() throws IllegalArgumentException {
+        User user = new User("login", "myemail@mail.ru");
+        Assertions.assertEquals("login", user.getLogin());
+        Assertions.assertEquals("myemail@mail.ru", user.getEmail());
+    }
+
+    @Test
+    void shouldGetEmail() {
+    }
 }
