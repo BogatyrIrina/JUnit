e26e0345ddb5ab9555819767d5b0995b035ec03a
admin <kochereva_irina@mail.ru>
2023-04-23 23:49:09 +0300

    Д/з на тему "Тестирование" выполнено

--- src/main/java/com/example/junit/User.java
+++ src/main/java/com/example/junit/User.java
@@ -1,16 +1,21 @@
 package com.example.junit;
 
+import java.util.Objects;
+
 public class User {
-    private final String login;
-    private final String email;
+    private String login;
+    private String email;
+
+    public User() {
+    }
 
     public User(String login, String email) throws IllegalArgumentException {
         if (login.equals(email)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Login is the same as email");
         }
         this.login = login;
-        if (!email.contains("@") || !email.contains(".")) {
-            throw new IllegalArgumentException();
+        if (email == null || !email.contains("@") || !email.contains(".")) {
+            throw new IllegalArgumentException("Incorrect email");
         }
         this.email = email;
     }
@@ -21,5 +26,18 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(login, user.login) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, email);
     }
 }
--- src/test/java/com/example/junit/UserTest.java
+++ src/test/java/com/example/junit/UserTest.java
@@ -6,24 +6,42 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 public class UserTest {
+    private static User user1;
+    private static User user2;
     @BeforeAll
     private static void initParams(){
         System.out.println("Start testing");
+        user1 = new User();
+        user2 = new User("login", "myemail@mail.ru");
     }
     @AfterAll
     private static void shouldDoAfterAll(){
         System.out.println("Finish testing");
     }
+
+    @Test
+    public void testCreateUserWithParameters(){
+        assertTrue(user2.getEmail() != null && user2.getLogin() != null);
+    }
+    @Test
+    public void testCreateUserWithoutParameters(){
+        assertTrue(user1.getEmail() == null && user1.getLogin() == null);
+    }
+
+
     @Test
     void shouldGetLogin() throws IllegalArgumentException {
         User user = new User("login", "myemail@mail.ru");
         Assertions.assertEquals("login", user.getLogin());
-        Assertions.assertEquals("myemail@mail.ru", user.getEmail());
     }
 
     @Test
     void shouldGetEmail() {
+        User user = new User("login", "myemail@mail.ru");
+        Assertions.assertEquals("myemail@mail.ru", user.getEmail());
     }
 }
